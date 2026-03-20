cadfe1181b0186b30368923aa7db3e2a635f538a
Beni Lev <beni@example.com>
2026-03-20 14:53:38 +0200

    Add build command example

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Wrapper for Armbian build with custom settings for Radxa Cubie A7Z
 #
-# Usage: ./build.sh [compile.sh args...]
-#   A7Z_ROOTPWD=mypass ./build.sh    # custom root password
+# Usage:
+#   ./build.sh build BOARD=radxa-cubie-a7z BRANCH=legacy BUILD_DESKTOP=no BUILD_MINIMAL=no KERNEL_CONFIGURE=no RELEASE=bookworm
+#
+# Options:
+#   A7Z_ROOTPWD=mypass ./build.sh ...    # custom root password
+#   DOCKER_SKIP=yes ./build.sh ...       # build natively (skip Docker)
 #
 
 declare -a DOCKER_EXTRA_ARGS=("--env" "PESTER_TERMINAL=no")
